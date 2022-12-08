df81a84599228cc717e2b5607fc79d9a2530c05b
tom-novak <hello@tom-novak.com>
2022-12-08 18:34:01 +0100

    Book grid tile preview

--- app/src/main/java/com/tom_novak/droidbookstore/ui/booksearch/BookSearchScreen.kt
+++ app/src/main/java/com/tom_novak/droidbookstore/ui/booksearch/BookSearchScreen.kt
@@ -2,18 +2,16 @@ package com.tom_novak.droidbookstore.ui.booksearch
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.tom_novak.droidbookstore.ui.model.Book
+import com.tom_novak.droidbookstore.ui.view.BookGridTile
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -29,9 +27,12 @@ fun BookSearchScreen(
             modifier = Modifier
                 .padding(padding)
         ) {
-            Card(modifier = Modifier.fillMaxWidth()) {
-                Text("Book search")
-            }
+            BookGridTile(
+                imgSrc = "https://itbook.store/img/books/9781617294136.png",
+                title = "New Book",
+                authors = "Fancy Author",
+                price = "$200.00",
+            )
         }
     }
 }
---
+++ app/src/main/java/com/tom_novak/droidbookstore/ui/composable/BookGridTile.kt
@@ -0,0 +1,85 @@
+package com.tom_novak.droidbookstore.ui.view
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.tom_novak.droidbookstore.R
+
+@Composable
+fun BookGridTile(
+    imgSrc: String? = null,
+    title: String? = null,
+    authors: String? = null,
+    price: String? = null,
+) {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        if (imgSrc != null)
+            AsyncImage(
+                model = imgSrc,
+                placeholder = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = "Fake image",
+                modifier = Modifier
+                    .size(width = 128.dp, height = 128.dp)
+            )
+
+        if (title != null) {
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = title,
+                maxLines = 1,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+
+        if (authors != null) {
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = authors,
+                maxLines = 1,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+
+        if (price != null) {
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = price,
+                maxLines = 1,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    }
+}
+
+@Preview(
+    name = "Light Mode",
+    showBackground = true,
+)
+@Preview(
+    name = "Dark Mode",
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+)
+@Composable
+fun PreviewBookGridTile() {
+    BookGridTile(
+        imgSrc = "https://itbook.store/img/books/9781617294136.png",
+        title = "New Book",
+        authors = "Fancy Author",
+        price = "$200.00",
+    )
+}
